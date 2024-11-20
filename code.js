7d88f638e64dc8c6de14f870c6b7e4da5427ee2e
Hsing Chen <twrandy24@gmail.com>
2024-11-20 16:18:14 +0800

    init

---
+++ code.js
@@ -0,0 +1,100 @@
+"use strict";
+figma.showUI(__html__);
+function getNodeStyles(node) {
+    const styles = {
+        // Layout
+        width: node.width,
+        height: node.height,
+        x: node.x,
+        y: node.y,
+        rotation: node.rotation,
+        layoutMode: node.layoutMode,
+        primaryAxisSizingMode: node.primaryAxisSizingMode,
+        counterAxisSizingMode: node.counterAxisSizingMode,
+        primaryAxisAlignItems: node.primaryAxisAlignItems,
+        counterAxisAlignItems: node.counterAxisAlignItems,
+        paddingTop: node.paddingTop,
+        paddingRight: node.paddingRight,
+        paddingBottom: node.paddingBottom,
+        paddingLeft: node.paddingLeft,
+        itemSpacing: node.itemSpacing,
+        // Appearance
+        opacity: node.opacity,
+        blendMode: node.blendMode,
+        isMask: node.isMask,
+        effects: node.effects,
+        effectStyleId: node.effectStyleId,
+        // Fill & Stroke
+        fills: node.fills,
+        fillStyleId: node.fillStyleId,
+        strokes: node.strokes,
+        strokeStyleId: node.strokeStyleId,
+        strokeWeight: node.strokeWeight,
+        strokeAlign: node.strokeAlign,
+        strokeCap: node.strokeCap,
+        strokeJoin: node.strokeJoin,
+        dashPattern: node.dashPattern,
+        // Corner
+        cornerRadius: node.cornerRadius,
+        topLeftRadius: node.topLeftRadius,
+        topRightRadius: node.topRightRadius,
+        bottomRightRadius: node.bottomRightRadius,
+        bottomLeftRadius: node.bottomLeftRadius,
+        // Constraints
+        constraints: node.constraints,
+        layoutAlign: node.layoutAlign,
+        layoutGrow: node.layoutGrow,
+    };
+    // Remove undefined properties
+    Object.keys(styles).forEach(key => {
+        if (styles[key] === undefined) {
+            delete styles[key];
+        }
+    });
+    return styles;
+}
+function compareStyles(style1, style2) {
+    const differences = {};
+    const allKeys = new Set([...Object.keys(style1), ...Object.keys(style2)]);
+    allKeys.forEach(key => {
+        const val1 = style1[key];
+        const val2 = style2[key];
+        if (JSON.stringify(val1) !== JSON.stringify(val2)) {
+            differences[key] = {
+                frame1: val1,
+                frame2: val2
+            };
+        }
+    });
+    return differences;
+}
+figma.on('selectionchange', () => {
+    const selection = figma.currentPage.selection;
+    if (selection.length === 2 &&
+        selection[0].type === 'FRAME' &&
+        selection[1].type === 'FRAME') {
+        const frame1Styles = getNodeStyles(selection[0]);
+        const frame2Styles = getNodeStyles(selection[1]);
+        // Log styles for debugging
+        console.log('Frame 1 styles:', frame1Styles);
+        console.log('Frame 2 styles:', frame2Styles);
+        // Compare styles
+        const styleDifferences = compareStyles(frame1Styles, frame2Styles);
+        console.log('Style differences:', styleDifferences);
+        // Send results to UI
+        figma.ui.postMessage({
+            type: 'style-comparison',
+            frame1: {
+                name: selection[0].name,
+                styles: frame1Styles
+            },
+            frame2: {
+                name: selection[1].name,
+                styles: frame2Styles
+            },
+            differences: styleDifferences
+        });
+    }
+});
+// Set a default size for the UI
+figma.ui.resize(400, 600);
